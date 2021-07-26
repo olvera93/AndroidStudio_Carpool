4d74524cb33fbb354e66746e4211914ca3756217
Stuart Garcia <gapstu@gmail.com>
2021-07-26 14:37:28 -0500

    Creacion de la pantalla ver_Perfil

---
+++ app/src/main/java/com/example/carpool/VerPerfil.kt
@@ -0,0 +1,11 @@
+package com.example.carpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_perfil)
+    }
+}
--- app/src/main/java/com/example/carpool/Welcome.kt
+++ app/src/main/java/com/example/carpool/Welcome.kt
@@ -1,11 +1,14 @@
 package com.example.carpool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class Welcome : AppCompatActivity() {
     private lateinit var text:TextView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +17,14 @@ class Welcome : AppCompatActivity() {
         val bundle = intent.extras
         val name= bundle?.getString(USER_NAME)
         text.text="Bienvenido $name"
+
+        button = findViewById(R.id.button2)
+
+        button.setOnClickListener {
+            val intent = Intent(this, VerPerfil::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
+        }
     }
-}
