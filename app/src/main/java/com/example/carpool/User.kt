9e7906cda4ce98e766e2b765bfb8ff89031fa272
Stuart Garcia <gapstu@gmail.com>
2021-07-27 17:18:29 -0500

    Correccion UI Ver_Perfil

--- app/src/main/java/com/example/carpool/User.kt
+++ app/src/main/java/com/example/carpool/User.kt
@@ -1,7 +1,7 @@
 package com.example.carpool
 
 class User(val user: String,val password:String){
-    val database : MutableMap<String,String> = mutableMapOf("admin" to "123")
+    private val database : MutableMap<String,String> = mutableMapOf("admin" to "123")
 
     fun addUser():Boolean{
         return if(!database.containsKey(user)){
