85ac9e49f44f9b29904cdbb1b3e3d688523e47d4
Stuart Garcia <73726431+StuartGa@users.noreply.github.com>
2021-09-23 17:21:00 -0500

    Cambios en BD

--- app/src/main/java/com/example/carpool/controllers/VerPerfil.kt
+++ app/src/main/java/com/example/carpool/controllers/VerPerfil.kt
@@ -1,17 +1,26 @@
 package com.example.carpool.controllers
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.carpool.MainActivity
 import com.example.carpool.R
 import com.example.carpool.model.User
-//import com.example.carpool.principalscreen
+import com.example.carpool.model.UserDb
+import com.example.carpool.model.Userdbclass
+import com.example.carpool.principalscreen
+import com.example.carpool.validateUser
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class VerPerfil : AppCompatActivity() {
     lateinit var EditNombre:EditText
@@ -20,24 +29,30 @@ class VerPerfil : AppCompatActivity() {
     lateinit var EditContra:EditText
     lateinit var actbtn:Button
 
+    //Inicio de sharedPreferences
+    lateinit var preferences: SharedPreferences
+
+    //Objeto User
+    private lateinit var manduser:Userdbclass
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_perfil)
+        preferences = getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE) //SE OBTIENEN LOS SHARED PREFERENCES DE MODO PRIVADO
 
-        val userDB: User = intent.getParcelableExtra("userDB")!!
+
         getSupportActionBar()?.setHomeButtonEnabled(true)
         EditUsuario=findViewById(R.id.edit_userP)
         EditContra=findViewById(R.id.edit_passwordP)
         EditNombre=findViewById(R.id.edit_nameP)
         EditTelefono=findViewById(R.id.edit_phoneP)
         actbtn = findViewById(R.id.Actualizar)
+        setValues()
+        dbOperation()
 
-        EditUsuario.setText(userDB.user)
-        EditNombre.setText(userDB.name)
-        EditContra.setText(userDB.password)
-        EditTelefono.setText(userDB.phone)
+
 
         EditNombre.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
@@ -64,15 +79,10 @@ class VerPerfil : AppCompatActivity() {
                     error = null
             }}
             else{
-                Toast.makeText(this, getString(R.string.update_data), Toast.LENGTH_LONG).show()
-                userDB.user = EditUsuario.text.toString()
-                userDB.name = EditNombre.text.toString()
-                userDB.password = EditContra.text.toString()
-                userDB.phone = EditTelefono.text.toString()
 
-                val intent = Intent(this, RequestTravel::class.java)
-                intent.putExtra("userDB",userDB)
-                startActivity(intent)
+                Toast.makeText(this, getString(R.string.update_data), Toast.LENGTH_LONG).show()
+                dbOperationUpdate()
+
                 }
 
             }
@@ -80,13 +90,61 @@ class VerPerfil : AppCompatActivity() {
 
     override fun finish() {
         val userDB: User = intent.getParcelableExtra("userDB")!!
-        val intent = Intent(this, RequestTravel::class.java)
+        val intent = Intent(this, principalscreen::class.java)
         intent.putExtra("userDB", userDB)
 
         // Activity finished ok, return the data
         setResult(RESULT_OK, intent)
         super.finish()
     }
+    fun setValues(){
+        //obtenemos los valores desde preferencias
+        val user = preferences.getString(MainActivity.USERP,"")
+
+        EditUsuario.setText(user)
+    }
+    private fun dbOperationUpdate() {
+        //Validacion si existe usuario en la base de datos de la clase User
+        val nameu=EditNombre.text.toString()
+        val usuu=EditUsuario.text.toString()
+        val pasu=EditContra.text.toString()
+        val phoneu=EditTelefono.text.toString()
+
+
+        val executor: ExecutorService = Executors.newSingleThreadExecutor()
+        executor.execute(Runnable {
+            val Userdbclass = UserDb
+                .getInstance(this)
+                ?.userDao()
+                    Userdbclass?.apply {updateUser(usuu,nameu,pasu,phoneu)  }
+
+
+        })}
+
+    private fun setValuesDrawer(userdbclass: Userdbclass) {
+        runOnUiThread {
+        EditUsuario.setText(userdbclass.Name)
+        EditContra.setText(userdbclass.Password)
+        EditNombre.setText(userdbclass.Name)
+        EditTelefono.setText(userdbclass.Phone)}
+
+    }
+    private fun dbOperation() {
+        //Validacion si existe usuario en la base de datos de la clase User
+        val executor: ExecutorService = Executors.newSingleThreadExecutor()
+        executor.execute(Runnable {
+            val userdbclass = UserDb
+                .getInstance(this)
+                ?.userDao()
+                ?.viewProfile(EditUsuario.text.toString()) as Userdbclass
+            Log.d("user", userdbclass.toString())
+            setValuesDrawer(userdbclass)
+        })
+
+    }
+
+
+
 
 
 }
